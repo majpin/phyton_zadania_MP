f73a93e15e9b24f41f98e0b28e21e559aa4e3f32
Maja Pindelska <maja.pindelska@gmail.com>
2022-04-04 21:51:55 +0200

    zadania domowe

--- zad_1.6.py
+++ zad_1.6.py
@@ -3,13 +3,14 @@ ilosc = int(input('Podaj ile biletow chcesz kupic?: '))
 
 if 0 <= wiek <= 6:
     cena = 0
-elif 7 <= wiek <=17:
+elif 17 >= wiek >= 7:
     cena = 2.28
-elif 18 <= wiek >= 64:
+elif 18 <= wiek <= 64:
     cena = 3.80
 else:
     cena = 1.90
 
 naleznosc = ilosc * cena
-print (f'do zaplacenia jest: {naleznosc}zl za {ilosc} biletow')
+print (f'do zaplacenia jest: {naleznosc} zl')
+
 
