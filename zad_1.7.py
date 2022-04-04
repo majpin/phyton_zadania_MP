607c2602541bb1939a417e440d36d87111b474e0
Maja Pindelska <maja.pindelska@gmail.com>
2022-04-04 21:33:56 +0200

    zadania domowe

--- zad_1.7.py
+++ zad_1.7.py
@@ -1,6 +1,6 @@
 towar = int(input('co chcesz kupic?: '))
 ile = int(input('ile chcesz kupic?: '))
 cena = int(input('ilr kosztuje dany produkt?: '))
-
-print(f'do zaplaty jest
+naleznosc = cena * ile
+print(f'do zaplaty jest{naleznosc} pln za {towar}')
 #nie wiem co tu jest źle nie rozumiem tego
