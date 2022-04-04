94ca070ce9212f6593b22e4917257f6f9e1eb4ad
Maja Pindelska <maja.pindelska@gmail.com>
2022-04-04 21:37:24 +0200

    zadania domowe

--- 1.8.py
+++ 1.8.py
@@ -1,6 +1,6 @@
 psie_lata = 5
 imie_psa = input('Podaj imie psa: ')
-wiek = input('Podaj wiek psa: ')
+wiek = int(input('Podaj wiek psa: '))
 
 print(f' Gdyby {imie_psa} byl czlowiekiem to mialby {wiek * psie_lata} lat.')
-#tutaj tez nie wiem czemu mi nie dziala..
+
