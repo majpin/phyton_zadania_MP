5fbb2401feb46242a592e7916527b0855acf1bd7
Maja Pindelska <maja.pindelska@gmail.com>
2022-04-04 21:31:21 +0200

    zadania domowe

---
+++ zad_1.7.py
@@ -0,0 +1,6 @@
+towar = int(input('co chcesz kupic?: '))
+ile = int(input('ile chcesz kupic?: '))
+cena = int(input('ilr kosztuje dany produkt?: '))
+
+print(f'do zaplaty jest
+#nie wiem co tu jest źle nie rozumiem tego
